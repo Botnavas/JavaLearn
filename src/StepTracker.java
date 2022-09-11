d7952c0dd625e2f99a2860312ea01ec7bbfd8a33
botnavas <maksymmelkov@yandex.ru>
2022-09-11 19:22:17 +0300

    worked with code style

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -16,34 +16,45 @@ public class StepTracker {
 
     void fillDays()
     {
-        for (int i = 0; i < 30; i++)
+        for (int i = 0; i < 30; i++) {
             days[i] = 0;
+        }
     }
 
-    int maximumStepsPerMonths() {
+    int findMaximumStepsPerMonths() {
         int maximum = 0;
+
         for (int i = 0; i < 30; i++) {
-            if (days[i] > maximum) maximum = days[i];
+            if (days[i] > maximum) {
+                maximum = days[i];
+            }
         }
         return maximum;
     }
 
-    int averageStep() {
+    int findAverageStep() {
         int average = 0;
-        for (int i = 0; i < 30; i++)
+
+        for (int i = 0; i < 30; i++) {
             average += days[i];
+        }
         return average / 30;
     }
 
-    int bestSeries() {
+    int findBestSeries() {
         int series = 0;
         int bestSeries = 0;
+
         for (int i = 0; i < 30; i++) {
             if (days[i] >= goal) {
                 for (int j = i; j < 30; j++) {
-                    if (days[j] >= goal) series++;
+                    if (days[j] >= goal) {
+                        series++;
+                    }
                     else {
-                        if (series > bestSeries) bestSeries = series;
+                        if (series > bestSeries) {
+                            bestSeries = series;
+                        }
                         i = j;
                         series = 0;
                         break;
@@ -54,44 +65,55 @@ public class StepTracker {
         return bestSeries;
     }
 
-    double calories(int day) {
+    double calculateCaloriesPerDay(int day) {
         return days[day] * caloriesPerStep;
     }
 
-    double km(int day) {
+    double calculateKmPerDay(int day) {
         return days[day] * stepLength;
     }
 
-    double totalBurnt() {
-        double Burnt = 0;
-        for (int i = 0; i < 30; i++)
-            Burnt += calories(i);
-        return Burnt;
+    double calculateTotalBurnt() {
+        double burnt = 0;
+
+        for (int i = 0; i < 30; i++) {
+            burnt += calculateCaloriesPerDay(i);
+        }
+        return burnt;
     }
 
-    double totalKm() {
+    double calculateTotalKm() {
         double km = 0;
-        for (int i = 0; i < 30; i++)
-            km += km(i);
+
+        for (int i = 0; i < 30; i++) {
+            km += calculateKmPerDay(i);
+        }
         return km;
     }
 
     void changeGoal() {
         System.out.println("Current goal is: " + goal + ". Print new goal");
         int newGoal = scanner.nextInt();
+
         if (newGoal > 0) {
             goal = newGoal;
             System.out.println("New goal is: " + goal + " steps");
         }
-        else System.out.println("Error: goal less than 0");
+        else  {
+            System.out.println("Error: goal less than 0");
+        }
     }
 
     void inputMonths() {
         System.out.println("Current month is: " + month+ ". Print number of month");
         int newMonth = scanner.nextInt();
-        if (newMonth > 0 && newMonth <= 12)
+
+        if (newMonth > 0 && newMonth <= 12) {
             month = newMonth;
-        else System.out.println("Error: incorrect month");
+        }
+        else {
+            System.out.println("Error: incorrect month");
+        }
     }
 
     void inputSteps() {
@@ -105,20 +127,25 @@ public class StepTracker {
                 System.out.println("Steps for " + day + "day is: " + steps);
                 days[day] = steps;
             }
-            else System.out.println("Error: amount of steps should be positive");
-        } else System.out.println("Error: incorrect day");
+            else {
+                System.out.println("Error: amount of steps should be positive");
+            }
+        } else {
+            System.out.println("Error: incorrect day");
+        }
     }
 
     void showStats()
     {
-        for (int i = 0 ; i < 30; i++)
+        for (int i = 0 ; i < 30; i++) {
             System.out.println("Day" + i + ": " + days[i] + "steps, calories burnt: "
-                    + calories(i) + " calories, distance was: " + km(i) + "km");
-        System.out.println("Best steps per day was: " + maximumStepsPerMonths() + " steps");
-        System.out.println("Best series was: " + bestSeries() + " days");
-        System.out.println("Average step per day: " + averageStep() + " steps");
-        System.out.println("Total distance was: " + totalKm() + " km");
-        System.out.println("Total calories burnt: " + totalBurnt() + " calories");
+                    + calculateCaloriesPerDay(i) + " calories, distance was: " + calculateKmPerDay(i) + "km");
+        }
+        System.out.println("Best steps per day was: " + findMaximumStepsPerMonths() + " steps");
+        System.out.println("Best series was: " + findBestSeries() + " days");
+        System.out.println("Average step per day: " + findAverageStep() + " steps");
+        System.out.println("Total distance was: " + calculateTotalKm() + " km");
+        System.out.println("Total calories burnt: " + calculateTotalBurnt() + " calories");
     }
 }
 
